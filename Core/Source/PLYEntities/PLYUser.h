52d185e86e93f1dae0a57d9409dde204345a2b18
Oliver Drobnik <oliver@cocoanetics.com>
2014-07-10 10:42:47 +0200

    cleaned up PLYUser

--- Core/Source/PLYEntities/PLYUser.h
+++ Core/Source/PLYEntities/PLYUser.h
@@ -11,84 +11,105 @@
 @class PLYAuditor;
 
 @interface PLYUser : PLYEntity
-{
-    // The class identifier.
-    NSString *Class;
-    // The object id.
-    NSString *Id;
-    // The version.
-    NSNumber *version;
-    
-    // The user who created the object.
-    PLYAuditor *createdBy;
-    // The timestamp when object was created.
-    NSNumber *createdTime;
-    
-    // The user who updated the object the last time.
-    PLYAuditor *updatedBy;
-    // The timestamp when object was updated the last time.
-    NSNumber *updatedTime;
-    
-    // The nickname of the user.
-    NSString *nickname;
-    
-    // The first name of the user.
-    NSString *firstName;
-    // The last name of the user.
-    NSString *lastName;
-    
-    // The email of the user.
-    NSString *email;
-    // The birthday of the user.
-    NSDate *birthday;
-    // The gender of the user.
-    NSString *gender;
-    
-    // The gamification points of the user.
-    NSNumber *points;
-    // A list of unlocked achievements.
-    NSArray *unlockedAchievements;
-    
-    // The counter for all user which are follower of this user.
-    NSNumber *followerCount;
-    // The counter for all user this user is following.
-    NSNumber *followingCount;
-    
-    // The url of the users avatar image. If no image is defined productlayer returns a gravatar image url.
-    NSString *avatarUrl;
-    
-    // Is this user following the logged in user.
-    bool following;
-    // Is this user followed the logged in user.
-    bool followed;
-}
+
+/**
+ The class identifier.
+ */
 @property (nonatomic, strong) NSString *Class;
+
+/**
+ The object id.
+ */
 @property (nonatomic, strong) NSString *Id;
+
+/**
+ The version.
+ */
 @property (nonatomic, strong) NSNumber *version;
 
+/**
+ The user who created the object.
+ */
 @property (nonatomic, strong) PLYAuditor *createdBy;
+
+/**
+ The timestamp when object was created.
+ */
 @property (nonatomic, strong) NSNumber *createdTime;
+
+/**
+ The user who updated the object the last time.
+ */
 @property (nonatomic, strong) PLYAuditor *updatedBy;
+
+/**
+ The timestamp when object was updated the last time.
+ */
 @property (nonatomic, strong) NSNumber *updatedTime;
 
+/**
+ The nickname of the user.
+ */
 @property (nonatomic, strong) NSString *nickname;
 
+/**
+ The first name of the user.
+ */
 @property (nonatomic, strong) NSString *firstName;
+
+/**
+ The last name of the user.
+ */
 @property (nonatomic, strong) NSString *lastName;
 
+/**
+ The email of the user.
+ */
 @property (nonatomic, strong) NSString *email;
+
+/**
+ The birthday of the user.
+ */
 @property (nonatomic, strong) NSDate *birthday;
+
+/**
+ The gender of the user.
+ */
 @property (nonatomic, strong) NSString *gender;
 
+/**
+ The gamification points of the user.
+ */
 @property (nonatomic, strong) NSNumber *points;
+
+/**
+ A list of unlocked achievements.
+ */
 @property (nonatomic, strong) NSArray *unlockedAchievements;
 
+/**
+ The counter for all user which are follower of this user.
+ */
 @property (nonatomic, strong) NSNumber *followerCount;
+
+/**
+ The counter for all user this user is following.
+ */
 @property (nonatomic, strong) NSNumber *followingCount;
 
+/**
+ The url of the users avatar image. If no image is defined productlayer returns a gravatar image url.
+ */
 @property (nonatomic, strong) NSString *avatarUrl;
 
-@property (nonatomic) bool following;
-@property (nonatomic) bool followed;
+/**
+ Is this user following the logged in user.
+ */
+@property (nonatomic) BOOL following;
+
+/**
+ Is this user followed the logged in user.
+ */
+@property (nonatomic) BOOL followed;
 
 @end
--- Core/Source/PLYEntities/PLYUser.m
+++ Core/Source/PLYEntities/PLYUser.m
@@ -14,199 +14,225 @@
 
 @implementation PLYUser
 
-@synthesize Class;
-@synthesize Id;
-@synthesize version;
-
-@synthesize createdBy;
-@synthesize createdTime;
-@synthesize updatedBy;
-@synthesize updatedTime;
-
-@synthesize nickname;
-
-@synthesize firstName;
-@synthesize lastName;
-
-@synthesize email;
-@synthesize birthday;
-@synthesize gender;
-
-@synthesize points;
-@synthesize unlockedAchievements;
-
-@synthesize followerCount;
-@synthesize followingCount;
-
-@synthesize avatarUrl;
-
-@synthesize followed;
-@synthesize following;
-
 + (NSString *)entityTypeIdentifier
 {
-    return @"com.productlayer.User";
+	return @"com.productlayer.User";
 }
 
-- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+- (void)setValue:(id)value forKey:(NSString *)key
 {
-	self = [super initWithDictionary:dictionary];
-	
-	if (self)
+	if ([key isEqualToString:@"pl-created-by"])
 	{
-		[self setAttributesFromDictionary:dictionary];
+		if ([value isKindOfClass:[NSDictionary class]])
+		{
+			self.createdBy = [[PLYAuditor alloc] initWithDictionary:value];
+		}
+	}
+	else if ([key isEqualToString:@"pl-app"] || [key isEqualToString:@"pl-usr-roles"])
+	{
+		// Do nothing
+	}
+	else if ([key isEqualToString:@"pl-upd-by"])
+	{
+		if ([value isKindOfClass:[NSDictionary class]])
+		{
+			self.updatedBy = [[PLYAuditor alloc] initWithDictionary:value];
+		}
+	}
+	else
+	{
+		[super setValue:value forKey:key];
+	}
+}
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key
+{
+	if ([key isEqualToString:@"pl-class"])
+	{
+		[self setValue:value forKey:@"Class"];
+	}
+	else if ([key isEqualToString:@"pl-id"])
+	{
+		[self setValue:value forKey:@"Id"];
+	}
+	else if ([key isEqualToString:@"pl-version"])
+	{
+		[self setValue:value forKey:@"version"];
+	}
+	else if ([key isEqualToString:@"pl-created-by"])
+	{
+		[self setValue:value forKey:@"createdBy"];
+	}
+	else if ([key isEqualToString:@"pl-created-time"])
+	{
+		[self setValue:value forKey:@"createdTime"];
+	}
+	else if ([key isEqualToString:@"pl-upd-by"])
+	{
+		[self setValue:value forKey:@"updatedBy"];
+	}
+	else if ([key isEqualToString:@"pl-upd-time"])
+	{
+		[self setValue:value forKey:@"updatedTime"];
+	}
+	else if ([key isEqualToString:@"pl-usr-nickname"])
+	{
+		[self setValue:value forKey:@"nickname"];
+	}
+	else if ([key isEqualToString:@"pl-usr-fname"])
+	{
+		[self setValue:value forKey:@"firstName"];
+	}
+	else if ([key isEqualToString:@"pl-usr-lname"])
+	{
+		[self setValue:value forKey:@"lastName"];
+	}
+	else if ([key isEqualToString:@"pl-usr-email"])
+	{
+		[self setValue:value forKey:@"email"];
+	}
+	else if ([key isEqualToString:@"pl-usr-bday"])
+	{
+		[self setValue:value forKey:@"birthday"];
+	}
+	else if ([key isEqualToString:@"pl-usr-gender"])
+	{
+		[self setValue:value forKey:@"gender"];
+	}
+	else if ([key isEqualToString:@"pl-usr-points"])
+	{
+		[self setValue:value forKey:@"points"];
+	}
+	else if ([key isEqualToString:@"pl-usr-achv_unlocked"])
+	{
+		[self setValue:value forKey:@"unlockedAchievements"];
+	}
+	else if ([key isEqualToString:@"pl-usr-follower_cnt"])
+	{
+		[self setValue:value forKey:@"followerCount"];
+	}
+	else if ([key isEqualToString:@"pl-usr-following_cnt"])
+	{
+		[self setValue:value forKey:@"followingCount"];
+	}
+	else if ([key isEqualToString:@"pl-usr-img"])
+	{
+		[self setValue:value forKey:@"avatarUrl"];
+	}
+	else if ([key isEqualToString:@"pl-usr-followed"])
+	{
+		self.followed = [(NSNumber *)value boolValue];
+	}
+	else if ([key isEqualToString:@"pl-usr-following"])
+	{
+		self.following = [(NSNumber *)value boolValue];
+	}
+}
+
+- (NSDictionary *)dictionaryRepresentation
+{
+	NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:1];
+	
+	if (_Class)
+	{
+		[dict setObject:_Class forKey:@"pl-class"];
 	}
 	
-	return self;
-}
-
-- (void)setAttributesFromDictionary:(NSDictionary *)aDictionary {
-    
-    if (![aDictionary isKindOfClass:[NSDictionary class]]) {
-        return;
-    }
-    
-    [self setValuesForKeysWithDictionary:aDictionary];
-    
-}
-
-- (void)setValue:(id)value forKey:(NSString *)key {
-    
-    if ([key isEqualToString:@"pl-created-by"]) {
-        
-        if ([value isKindOfClass:[NSDictionary class]]) {
-            self.createdBy = [[PLYAuditor alloc] initWithDictionary:value];
-        }
-        
-    } else if ([key isEqualToString:@"pl-app"] || [key isEqualToString:@"pl-usr-roles"]) {
-        // Do nothing
-    } else if ([key isEqualToString:@"pl-upd-by"]) {
-        
-        if ([value isKindOfClass:[NSDictionary class]]) {
-            self.updatedBy = [[PLYAuditor alloc] initWithDictionary:value];
-        }
-    } else {
-        [super setValue:value forKey:key];
-    }
-}
-
-
-- (void)setValue:(id)value forUndefinedKey:(NSString *)key {
-    
-    if ([key isEqualToString:@"pl-class"]) {
-        [self setValue:value forKey:@"Class"];
-    } else if ([key isEqualToString:@"pl-id"]) {
-        [self setValue:value forKey:@"Id"];
-    }  else if ([key isEqualToString:@"pl-version"]) {
-        [self setValue:value forKey:@"version"];
-    } else if ([key isEqualToString:@"pl-created-by"]) {
-        [self setValue:value forKey:@"createdBy"];
-    } else if ([key isEqualToString:@"pl-created-time"]) {
-        [self setValue:value forKey:@"createdTime"];
-    } else if ([key isEqualToString:@"pl-upd-by"]) {
-        [self setValue:value forKey:@"updatedBy"];
-    } else if ([key isEqualToString:@"pl-upd-time"]) {
-        [self setValue:value forKey:@"updatedTime"];
-    }
-    
-    else if ([key isEqualToString:@"pl-usr-nickname"]) {
-        [self setValue:value forKey:@"nickname"];
-    } else if ([key isEqualToString:@"pl-usr-fname"]) {
-        [self setValue:value forKey:@"firstName"];
-    }  else if ([key isEqualToString:@"pl-usr-lname"]) {
-        [self setValue:value forKey:@"lastName"];
-    } else if ([key isEqualToString:@"pl-usr-email"]) {
-        [self setValue:value forKey:@"email"];
-    } else if ([key isEqualToString:@"pl-usr-bday"]) {
-        [self setValue:value forKey:@"birthday"];
-    } else if ([key isEqualToString:@"pl-usr-gender"]) {
-        [self setValue:value forKey:@"gender"];
-    } else if ([key isEqualToString:@"pl-usr-points"]) {
-        [self setValue:value forKey:@"points"];
-    } else if ([key isEqualToString:@"pl-usr-achv_unlocked"]) {
-        [self setValue:value forKey:@"unlockedAchievements"];
-    } else if ([key isEqualToString:@"pl-usr-follower_cnt"]) {
-        [self setValue:value forKey:@"followerCount"];
-    } else if ([key isEqualToString:@"pl-usr-following_cnt"]) {
-        [self setValue:value forKey:@"followingCount"];
-    } else if ([key isEqualToString:@"pl-usr-img"]) {
-        [self setValue:value forKey:@"avatarUrl"];
-    } else if ([key isEqualToString:@"pl-usr-followed"]) {
-        followed = [(NSNumber *)value boolValue];
-    } else if ([key isEqualToString:@"pl-usr-following"]) {
-        following = [(NSNumber *)value boolValue];
-    }
-}
-
-- (NSDictionary *) dictionaryRepresentation{
-    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:1];
-    
-    if (Class) {
-        [dict setObject:Class forKey:@"pl-class"];
-    }
-    if (Id) {
-        [dict setObject:Id forKey:@"pl-id"];
-    }
-    if (version) {
-        [dict setObject:version forKey:@"pl-version"];
-    }
-    if (createdBy) {
-        [dict setObject:[createdBy dictionaryRepresentation] forKey:@"pl-created-by"];
-    }
-    if (createdTime) {
-        [dict setObject:createdTime forKey:@"pl-created-time"];
-    }
-    if (updatedBy) {
-        [dict setObject:[updatedBy dictionaryRepresentation] forKey:@"pl-upd-by"];
-    }
-    if (updatedTime) {
-        [dict setObject:updatedTime forKey:@"pl-upd-time"];
-    }
-    
-    if (nickname) {
-        [dict setObject:nickname forKey:@"pl-usr-nickname"];
-    }
-    if (firstName) {
-        [dict setObject:firstName forKey:@"pl-usr-fname"];
-    }
-    if (lastName) {
-        [dict setObject:lastName forKey:@"pl-usr-lname"];
-    }
-    if (email) {
-        [dict setObject:email forKey:@"pl-usr-email"];
-    }
-    if (birthday) {
-        [dict setObject:birthday forKey:@"pl-usr-bday"];
-    }
-    if (gender) {
-        [dict setObject:gender forKey:@"pl-usr-gender"];
-    }
-    if (points) {
-        [dict setObject:points forKey:@"pl-usr-points"];
-    }
-    if (unlockedAchievements) {
-        [dict setObject:unlockedAchievements forKey:@"pl-usr-achv_unlocked"];
-    }
-    if (followerCount) {
-        [dict setObject:followerCount forKey:@"pl-usr-follower_cnt"];
-    }
-
-    if (followingCount) {
-        [dict setObject:followingCount forKey:@"pl-usr-following_cnt"];
-    }
-
-    if (avatarUrl) {
-        [dict setObject:avatarUrl forKey:@"pl-usr-img"];
-    }
-    
-    if(following){
-        [dict setObject:[NSNumber numberWithBool:following] forKey:@"pl-usr-following"];
-    }
-    
-    if(followed){
-        [dict setObject:[NSNumber numberWithBool:followed] forKey:@"pl-usr-followed"];
-    }
-    
-    return dict;
+	if (_Id)
+	{
+		[dict setObject:_Id forKey:@"pl-id"];
+	}
+	
+	if (_version)
+	{
+		[dict setObject:_version forKey:@"pl-version"];
+	}
+	
+	if (_createdBy)
+	{
+		[dict setObject:[_createdBy dictionaryRepresentation] forKey:@"pl-created-by"];
+	}
+	
+	if (_createdTime)
+	{
+		[dict setObject:_createdTime forKey:@"pl-created-time"];
+	}
+	
+	if (_updatedBy)
+	{
+		[dict setObject:[_updatedBy dictionaryRepresentation] forKey:@"pl-upd-by"];
+	}
+	
+	if (_updatedTime)
+	{
+		[dict setObject:_updatedTime forKey:@"pl-upd-time"];
+	}
+	
+	if (_nickname)
+	{
+		[dict setObject:_nickname forKey:@"pl-usr-nickname"];
+	}
+	
+	if (_firstName)
+	{
+		[dict setObject:_firstName forKey:@"pl-usr-fname"];
+	}
+	
+	if (_lastName)
+	{
+		[dict setObject:_lastName forKey:@"pl-usr-lname"];
+	}
+	
+	if (_email)
+	{
+		[dict setObject:_email forKey:@"pl-usr-email"];
+	}
+	
+	if (_birthday)
+	{
+		[dict setObject:_birthday forKey:@"pl-usr-bday"];
+	}
+	
+	if (_gender)
+	{
+		[dict setObject:_gender forKey:@"pl-usr-gender"];
+	}
+	
+	if (_points)
+	{
+		[dict setObject:_points forKey:@"pl-usr-points"];
+	}
+	
+	if (_unlockedAchievements)
+	{
+		[dict setObject:_unlockedAchievements forKey:@"pl-usr-achv_unlocked"];
+	}
+	
+	if (_followerCount)
+	{
+		[dict setObject:_followerCount forKey:@"pl-usr-follower_cnt"];
+	}
+	
+	if (_followingCount)
+	{
+		[dict setObject:_followingCount forKey:@"pl-usr-following_cnt"];
+	}
+	
+	if (_avatarUrl)
+	{
+		[dict setObject:_avatarUrl forKey:@"pl-usr-img"];
+	}
+	
+	if (_following)
+	{
+		[dict setObject:[NSNumber numberWithBool:_following] forKey:@"pl-usr-following"];
+	}
+	
+	if (_followed)
+	{
+		[dict setObject:[NSNumber numberWithBool:_followed] forKey:@"pl-usr-followed"];
+	}
+	
+	return dict;
 }
 @end
