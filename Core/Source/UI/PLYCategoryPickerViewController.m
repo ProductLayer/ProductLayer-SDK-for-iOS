3dd0cf745480d28c17aac5b440e86cc04f6c0407
Oliver Drobnik <oliver@cocoanetics.com>
2014-11-11 19:29:35 +0100

    Added file caching of categories and default category setup

--- Core/Source/UI/PLYCategoryPickerViewController.m
+++ Core/Source/UI/PLYCategoryPickerViewController.m
@@ -9,6 +9,7 @@
 #import "PLYCategoryPickerViewController.h"
 #import "PLYServer.h"
 #import "DTLog.h"
+#import "NSString+DTPaths.h"
 
 #import <ProductLayer/ProductLayer.h>
 
@@ -31,26 +32,19 @@ NSArray *_sortedKeys = nil;
 {
 	[super viewDidLoad];
 	
-	NSString *language = [[NSLocale currentLocale] localeIdentifier];
-	[self.productLayerServer getCategoriesForLocale:language completion:^(id result, NSError *error) {
-		if (!result)
-		{
-			DTLogError(@"Error loading categories from server: %@", error);
-			return;
-		}
-		
-		dispatch_async(dispatch_get_main_queue(), ^{
-			[self _updateCategories:result];
-		});
-	}];
-	
+	// use normal cells
 	[self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CELL_IDENTIFIER];
 	
+	// setup search controller
 	_searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
 	_searchController.searchResultsUpdater = self;
 	_searchController.searchBar.frame = CGRectMake(0.0, 0.0, CGRectGetWidth(self.tableView.frame), 44.0); // need frame or else it is invisible
 	_searchController.dimsBackgroundDuringPresentation = NO;
+	
 	self.tableView.tableHeaderView = _searchController.searchBar;
+	
+	// load last category list we had
+	[self _loadCategoriesFromCache];
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -75,6 +69,13 @@ NSArray *_sortedKeys = nil;
 	}
 	
 	[self _selectRowForCategoryKey:_selectedCategoryKey animated:NO];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+	[super viewDidAppear:animated];
+	
+	[self _updateCategoriesFromServer];
 }
 
 - (void)_setupForTraitCollection:(UITraitCollection *)collection
@@ -121,6 +122,129 @@ NSArray *_sortedKeys = nil;
 	[self.tableView reloadData];
 	
 	[self _selectRowForCategoryKey:_selectedCategoryKey animated:YES];
+}
+
+- (void)_setupDefaultCategories
+{
+	NSArray *categories = [NSMutableArray arrayWithObjects:
+								  @"pl-prod-cat-magazines",
+								  @"pl-prod-cat-books",
+								  @"pl-prod-cat-books-children_and_teenagers",
+								  @"pl-prod-cat-books-comics_and_picturestories",
+								  @"pl-prod-cat-books-computer_and_internet",
+								  @"pl-prod-cat-books-economy_investments",
+								  @"pl-prod-cat-books-health_mind_and_body",
+								  @"pl-prod-cat-books-mystery_suspense",
+								  @"pl-prod-cat-books-novel_stories",
+								  @"pl-prod-cat-books-religious_spirituality",
+								  @"pl-prod-cat-books-romance",
+								  @"pl-prod-cat-books-sciencefiction_and_fantasy",
+								  @"pl-prod-cat-clothes",
+								  @"pl-prod-cat-clothes-jeans",
+								  @"pl-prod-cat-clothes-pants",
+								  @"pl-prod-cat-clothes-shirts",
+								  @"pl-prod-cat-clothes-suites",
+								  @"pl-prod-cat-clothes-underwear",
+								  @"pl-prod-cat-drinks",
+								  @"pl-prod-cat-drinks-alcoholic",
+								  @"pl-prod-cat-drinks-non_alcoholic",
+								  @"pl-prod-cat-electronic",
+								  @"pl-prod-cat-electronic-camera",
+								  @"pl-prod-cat-electronic-computer",
+								  @"pl-prod-cat-electronic-computer-notebook",
+								  @"pl-prod-cat-electronic-computer-tablet",
+								  @"pl-prod-cat-electronic-computer-systems",
+								  @"pl-prod-cat-electronic-computer-components",
+								  @"pl-prod-cat-electronic-computer-audio",
+								  @"pl-prod-cat-electronic-computer-video",
+								  @"pl-prod-cat-electronic-computer-mainboard",
+								  @"pl-prod-cat-electronic-computer-monitor",
+								  @"pl-prod-cat-electronic-computer-network",
+								  @"pl-prod-cat-electronic-computer-cpu",
+								  @"pl-prod-cat-electronic-computer-ram",
+								  @"pl-prod-cat-electronic-computer-storage",
+								  @"pl-prod-cat-electronic-computer-graphic_card",
+								  @"pl-prod-cat-electronic-computer-input_device",
+								  @"pl-prod-cat-electronic-computer-cable",
+								  @"pl-prod-cat-electronic-phones",
+								  @"pl-prod-cat-electronic-tv",
+								  @"pl-prod-cat-electronic-wearables",
+								  @"pl-prod-cat-electronic-printer_and_scanner",
+								  @"pl-prod-cat-electronic-printer_and_scanner-3dprinter",
+								  @"pl-prod-cat-electronic-printer_and_scanner-scanner",
+								  @"pl-prod-cat-electronic-printer_and_scanner-printer",
+								  @"pl-prod-cat-electronic-printer_and_scanner-barcode_printer",
+								  @"pl-prod-cat-electronic-printer_and_scanner-barcode_scanner",
+								  @"pl-prod-cat-electronic-printer_and_scanner-scanner_accessories",
+								  @"pl-prod-cat-electronic-printer_and_scanner-printer_accessories",
+								  @"pl-prod-cat-food",
+								  @"pl-prod-cat-food-dairy",
+								  @"pl-prod-cat-food-fruit",
+								  @"pl-prod-cat-food-grains",
+								  @"pl-prod-cat-food-meat",
+								  @"pl-prod-cat-food-sweets",
+								  @"pl-prod-cat-food-vegetables",
+								  @"pl-prod-cat-food-instant_meal",
+								  @"pl-prod-cat-health_and_personal_care",
+								  @"pl-prod-cat-health_and_personal_care-beauty",
+								  @"pl-prod-cat-home_and_kitchen",
+								  @"pl-prod-cat-industrial_and_scientific",
+								  @"pl-prod-cat-others", nil];
+	
+	NSMutableDictionary *tmpDict = [NSMutableDictionary dictionary];
+	
+	for (NSString *oneKey in categories)
+	{
+		NSString *text = PLYLocalizedStringFromTable(oneKey, @"API", @"Product Category Key");
+		tmpDict[oneKey] = text;
+	}
+	
+	[self _updateCategories:tmpDict];
+	[self _saveCategoriesInCache];
+}
+
+- (void)_saveCategoriesInCache
+{
+	NSString *path = [[NSString cachesPath] stringByAppendingString:@"ProductCategories.plist"];
+	[_categoryDictionary writeToFile:path atomically:YES];
+}
+
+- (void)_loadCategoriesFromCache
+{
+	NSString *path = [[NSString cachesPath] stringByAppendingString:@"ProductCategories.plist"];
+	NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:path];
+	
+	if (dictionary)
+	{
+		[self _updateCategories:dictionary];
+	}
+	else
+	{
+		[self _setupDefaultCategories];
+	}
+}
+
+- (void)_updateCategoriesFromServer
+{
+	NSString *language = [[NSLocale currentLocale] localeIdentifier];
+	[self.productLayerServer getCategoriesForLocale:language completion:^(id result, NSError *error) {
+		if ([error.domain isEqualToString:NSURLErrorDomain] && error.code == kCFURLErrorNotConnectedToInternet)
+		{
+			// no Internet
+			return;
+		}
+		
+		if (!result)
+		{
+			DTLogError(@"Error loading categories from server: %@", error);
+			return;
+		}
+		
+		dispatch_async(dispatch_get_main_queue(), ^{
+			[self _updateCategories:result];
+			[self _saveCategoriesInCache];
+		});
+	}];
 }
 
 - (BOOL)_text:(NSString *)text containsAllTerms:(NSArray *)terms
