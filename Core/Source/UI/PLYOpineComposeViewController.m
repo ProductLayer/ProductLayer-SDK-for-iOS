8d8f6bf7e742e054406c12d59bf55db2e48d6c84
Oliver Drobnik <oliver@cocoanetics.com>
2014-11-21 21:23:17 +0100

    Added social cross-posting support

--- Core/Source/UI/PLYOpineComposeViewController.m
+++ Core/Source/UI/PLYOpineComposeViewController.m
@@ -22,6 +22,14 @@
 	
 	NSString *_text;
 	NSString *_language;
+	
+	UIButton *_twitterButton;
+	UIButton *_facebookButton;
+	
+	UIEdgeInsets _insets;
+	
+	BOOL _postToTwitter;
+	BOOL _postToFacebook;
 }
 
 - (instancetype)initWithOpine:(PLYOpine *)opine
@@ -32,9 +40,60 @@
 	{
 		_text = opine.text;
 		_language = opine.language;
+		
+		_postToTwitter = opine.shareOnTwitter;
+		_postToFacebook = opine.shareOnFacebook;
 	}
 	
 	return self;
+}
+
+- (void)dealloc
+{
+	[self.productLayerServer removeObserver:self forKeyPath:@"loggedInUser"];
+	[[NSNotificationCenter  defaultCenter] removeObserver:self];
+}
+
+- (void)_updateSocialButtons
+{
+	if ([self.productLayerServer.loggedInUser.socialConnections[@"twitter"] boolValue])
+	{
+		_twitterButton.enabled = YES;
+	}
+	else
+	{
+		_postToTwitter = NO;
+		_twitterButton.enabled = NO;
+	}
+	
+	if ([self.productLayerServer.loggedInUser.socialConnections[@"facebook"] boolValue])
+	{
+		_facebookButton.enabled = YES;
+	}
+	else
+	{
+		_postToFacebook = NO;
+		_facebookButton.enabled = NO;
+	}
+
+	
+	if (_postToFacebook)
+	{
+		_facebookButton.tintColor = PLYBrandColor();
+	}
+	else
+	{
+		_facebookButton.tintColor = [UIColor grayColor];
+	}
+	
+	if (_postToTwitter)
+	{
+		_twitterButton.tintColor = PLYBrandColor();
+	}
+	else
+	{
+		_twitterButton.tintColor = [UIColor grayColor];
+	}
 }
 
 - (void)loadView
@@ -49,8 +108,27 @@
 	_textView.layer.borderColor = PLYBrandColor().CGColor;
 	_textView.layer.borderWidth = 1;
 	_textView.layer.cornerRadius = 10;
+	_textView.scrollIndicatorInsets = UIEdgeInsetsMake(5, 0, 5, 0);
 	
 	[view addSubview:_textView];
+
+	NSString *twitterPath = [PLYResourceBundle() pathForResource:@"twitter" ofType:@"png"];
+	UIImage *twitterIcon = [[UIImage imageWithContentsOfFile:twitterPath] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+	_twitterButton = [UIButton buttonWithType:UIButtonTypeCustom];
+	_twitterButton.frame = CGRectMake(0, 100, 50, 50);
+	[_twitterButton setImage:twitterIcon forState:UIControlStateNormal];
+	[_twitterButton addTarget:self action:@selector(_handleTwitter:) forControlEvents:UIControlEventTouchUpInside];
+	[view addSubview:_twitterButton];
+	
+	NSString *facebookPath = [PLYResourceBundle() pathForResource:@"facebook" ofType:@"png"];
+	UIImage *facebookIcon = [[UIImage imageWithContentsOfFile:facebookPath] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+	_facebookButton = [UIButton buttonWithType:UIButtonTypeCustom];
+	_facebookButton.frame = CGRectMake(100, 100, 50, 50);
+	[_facebookButton setImage:facebookIcon forState:UIControlStateNormal];
+	[_facebookButton addTarget:self action:@selector(_handleFacebook:) forControlEvents:UIControlEventTouchUpInside];
+	[view addSubview:_facebookButton];
+	
+	[self _updateSocialButtons];
 	
 	self.view = view;
 	
@@ -69,6 +147,17 @@
 	}
 	
 	self.automaticallyAdjustsScrollViewInsets = NO;
+	
+	_insets = UIEdgeInsetsZero;
+	
+	NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+	[center addObserver:self selector:@selector(keyboardWillShow:)
+						name:UIKeyboardWillShowNotification object:nil];
+	[center addObserver:self selector:@selector(keyboardWillHide:)
+						name:UIKeyboardWillHideNotification object:nil];
+	
+	// observe the logged in user
+	[self.productLayerServer addObserver:self forKeyPath:@"loggedInUser" options:NSKeyValueObservingOptionNew context:NULL];
 }
 
 - (void)viewWillLayoutSubviews
@@ -78,7 +167,9 @@
 	id<UILayoutSupport>top = [self topLayoutGuide];
 	id<UILayoutSupport>bottom = [self bottomLayoutGuide];
 	
-	_textView.frame = CGRectMake(10, [top length]+10, self.view.bounds.size.width-20, self.view.bounds.size.height - [top length] - [bottom length] - 20 );
+	_textView.frame = CGRectMake(10+_insets.left, [top length]+10+_insets.top, self.view.bounds.size.width-20-_insets.left - _insets.right, self.view.bounds.size.height - [top length] - [bottom length] - 20 - _insets.bottom - 40);
+	_facebookButton.frame = CGRectMake( CGRectGetMaxX(_textView.frame) - 50, CGRectGetMaxY(_textView.frame), 50, 50);
+	_twitterButton.frame = CGRectMake( CGRectGetMaxX(_textView.frame) - 100, CGRectGetMaxY(_textView.frame), 50, 50);
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -152,6 +243,8 @@
 		opine = [[PLYOpine alloc] init];
 		opine.text = _textView.text;
 		opine.language = _language;
+		opine.shareOnFacebook = _postToFacebook;
+		opine.shareOnTwitter = _postToTwitter;
 	}
 	
 	if ([_delegate respondsToSelector:@selector(opineComposeViewController:didFinishWithOpine:)])
@@ -160,6 +253,73 @@
 	}
 	
 	[self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+- (void)_handleTwitter:(id)sender
+{
+	_postToTwitter = !_postToTwitter;
+	[self _updateSocialButtons];
+}
+
+- (void)_handleFacebook:(id)sender
+{
+	_postToFacebook = !_postToFacebook;
+	[self _updateSocialButtons];
+}
+
+#pragma mark - Notifications
+
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+	// keyboard frame is in window coordinates
+	NSDictionary *userInfo = [notification userInfo];
+	CGRect keyboardFrame = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+ 
+	// convert own frame to window coordinates, frame is in superview's coordinates
+	CGRect ownFrame = [self.view.window convertRect:self.view.frame fromView:self.view.superview];
+ 
+	// calculate the area of own frame that is covered by keyboard
+	CGRect coveredFrame = CGRectIntersection(ownFrame, keyboardFrame);
+ 
+	// now this might be rotated, so convert it back
+	coveredFrame = [self.view.window convertRect:coveredFrame toView:self.view.superview];
+ 
+	// set inset to make up for covered array at bottom
+	_insets = UIEdgeInsetsMake(0, 0, coveredFrame.size.height, 0);
+	
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:[notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]];
+	[UIView setAnimationCurve:[notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+	[UIView setAnimationBeginsFromCurrentState:YES];
+	
+	// work
+	[self.view setNeedsLayout];
+	[self.view layoutIfNeeded];
+	
+	[UIView commitAnimations];
+	
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+	// set inset to make up for no longer covered array at bottom
+	_insets = UIEdgeInsetsMake(0, 0, 0, 0);
+	
+	[UIView beginAnimations:nil context:NULL];
+	[UIView setAnimationDuration:[notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue]];
+	[UIView setAnimationCurve:[notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue]];
+	[UIView setAnimationBeginsFromCurrentState:YES];
+	
+	// work
+	[self.view setNeedsLayout];
+	[self.view layoutIfNeeded];
+	
+	[UIView commitAnimations];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	[self _updateSocialButtons];
 }
 
 #pragma mark - UITextViewDelegate
@@ -191,6 +351,9 @@
 {
 	_text = opine.text;
 	_language = opine.language;
+	
+	_postToTwitter = opine.shareOnTwitter;
+	_postToFacebook = opine.shareOnFacebook;
 }
 
 @end
