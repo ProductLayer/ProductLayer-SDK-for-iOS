2cd81e484c402a7dd3d7b07872d89571ead09db3
Oliver Drobnik <oliver@cocoanetics.com>
2014-11-30 13:41:01 +0100

    removed key

--- Demo/Source/ProductLayerConfig.h
+++ Demo/Source/ProductLayerConfig.h
@@ -3,7 +3,7 @@
 // sign up for a developer account at http://api.productlayer.com, create an API key for your app
 // and paste it below, removing the comment slashes
 
-#define PLY_API_KEY @"b75d1c91-52d7-4ffc-bd1a-7d94f43a82ac"
+// #define PLY_API_KEY @"00000000-0000-0000-0000-000000000000"
 
 #ifndef PLY_API_KEY
 #error Please create an API key and add it to ProductLayerConfig.h
